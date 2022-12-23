5472b2cb83f2649500cd5a3fce533668ff1ba5e2
Thaís Silva <thaisdss.silva@gmail.com>
2022-12-22 23:52:14 -0300

    update notifications-service

---
+++ src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,20 @@
+import { SendNotification } from './send-notification';
+import { Notification } from '../entities/notification';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
+
+describe('Send notification', () => {
+  it('should be able to send notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+
+    const sendNotification = new SendNotification(notificationsRepository);
+
+    const { notification } = await sendNotification.execute({
+      content: 'This is a notification',
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
+  });
+});
